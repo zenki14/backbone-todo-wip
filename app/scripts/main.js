18586e4811bf38267c4fd68b31a88a96fd486ba0
Dane Worsham <zenki14@gmail.com>
2015-02-13 10:22:06 -0500

    styled and some js added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,51 @@
-console.log('The Iron Yard Rocks');
+var Task = Backbone.Model.extend({
+
+	initialize: function () {
+		console.log('Task Created!');
+	},
+
+	idAttribute: '_id', //MEGA important.
+
+	defaults: {
+		title: '',
+		completed: false
+	},
+
+	toggle: function () {
+		this.save({
+			completed: !this.get('completed')
+		});
+	}
+
+});
+
+var Todos = Backbone.Collection.extend({
+
+
+	initialize: function () {
+		console.log('Collection Created');
+	},
+
+	model: Task,
+	url: 'http://tiy-atl-fe-server.herokuapp.com/collections/daneTodo'
+
+
+
+
+
+});
+
+//instances
+
+var allTasks = new Todos();
+
+allTasks.fetch().done( function () {
+	console.log('Tasks Fetched');
+});
+
+//tasks
+
+
+// var a = new Student ({ name: 'Spencer' });
+// var b = new Student ({ name: 'Alex' });
+// var c = new Student ({ name: 'Mark' });
