a34d99c72935ae2208f0c139aee2a56d4148937f
Dane Worsham <zenki14@gmail.com>
2015-02-16 08:49:20 -0500

    trying to figure out collection creation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,51 +1,73 @@
-var Task = Backbone.Model.extend({
+var Todo = Backbone.Model.extend({
 
-	initialize: function () {
-		console.log('Task Created!');
-	},
+	// initialize: function () {
+	// 	console.log('Task Created!');
+	// },
 
 	idAttribute: '_id', //MEGA important.
 
 	defaults: {
 		title: '',
 		completed: false
+
 	},
 
-	toggle: function () {
-		this.save({
-			completed: !this.get('completed')
-		});
-	}
+	// toggle: function () {
+	// 	this.save({
+	// 		completed: !this.get('completed')
+	// 	});
+	// }
 
 });
 
 var Todos = Backbone.Collection.extend({
 
 
-	initialize: function () {
-		console.log('Collection Created');
-	},
+	// initialize: function () {
+	// 	console.log('Collection Created');
+	// },
 
-	model: Task,
-	url: 'http://tiy-atl-fe-server.herokuapp.com/collections/daneTodo'
-
-
-
-
+	model: Todo,
+	url: 'http://tiy-atl-fe-server.herokuapp.com/collections/daneTodo',
 
 });
+
+//jquery
+
+var todos = new Todos;
+var todoForm = $('#main');
+var todoList = $('#todo-list');
+
+
+
+
+
+todoForm.on('keydown', 'input', function(e) {
+  if(e.keyCode == 13) {
+    e.preventDefault();
+    
+    var newTask = new Task( { task: $(this).val() });
+    if (newTask === '') { return; }
+    
+    todos.add(newTask);
+    newTask.save();
+    
+    $(this).val('');
+  }
+});
+
 
 //instances
 
-var allTasks = new Todos();
+// var allTasks = new Todos();
 
-allTasks.fetch().done( function () {
-	console.log('Tasks Fetched');
-});
+// allTasks.fetch().done( function () {
+// 	console.log('Tasks Fetched');
+// });
 
 //tasks
+todos.fetch();
 
-
-// var a = new Student ({ name: 'Spencer' });
+// var a = new Task ({ name: 'Spencer' });
 // var b = new Student ({ name: 'Alex' });
 // var c = new Student ({ name: 'Mark' });
